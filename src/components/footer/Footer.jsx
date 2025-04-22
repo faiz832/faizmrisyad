b19f0c644e63abfb87045387577e5443ab6917d3
faiz832 <faqodefazz@gmail.com>
2025-04-22 18:07:35 +0700

    fix footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -61,14 +61,9 @@ export default function Footer() {
         </div>
       </div>
       <div className="z-10 flex flex-col justify-between gap-4 sm:flex-row">
-        <p className="mb-16 text-center text-sm sm:mb-0 sm:text-left">
-          Design Inspired By{" "}
-          <a href="https://www.instagram.com/devraj_uiux/" target="_blank" className="hover:underline">
-            @devraj_uiux
-          </a>
-        </p>
+        <p className="mb-16 text-center text-sm sm:mb-0 sm:text-left">You've reached the bottom :)</p>
         <div className="sm:flex justify-center items-center opacity-container hidden gap-6 text-text-secondary">
-          <a aria-label="LinkedIn" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/devraj-chatribin/">
+          <a aria-label="LinkedIn" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/faizrisyad">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
               <rect width="4" height="12" x="2" y="9"></rect>
@@ -76,14 +71,14 @@ export default function Footer() {
             </svg>
           </a>
 
-          <a aria-label="GitHub" target="_blank" rel="noopener noreferrer" href="https://github.com/DevrajDC">
+          <a aria-label="GitHub" target="_blank" rel="noopener noreferrer" href="https://github.com/faiz832">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
               <path d="M9 18c-4.51 2-5-2-7-2"></path>
             </svg>
           </a>
 
-          <a aria-label="Instagram" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/devraj_uiux/">
+          <a aria-label="Instagram" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/faizmr_d">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
               <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
@@ -91,7 +86,7 @@ export default function Footer() {
             </svg>
           </a>
 
-          <a aria-label="Gmail" target="_blank" rel="noopener noreferrer" href="mailto:devrajchatribin9978@gmail.com">
+          <a aria-label="Gmail" target="_blank" rel="noopener noreferrer" href="mailto:risyad832@gmail.com">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <rect width="20" height="16" x="2" y="4" rx="2"></rect>
               <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
