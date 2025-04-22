0f126f14cb34911bd88d31c17703a83c2643dcb6
faiz832 <faqodefazz@gmail.com>
2025-04-23 05:19:21 +0700

    add hover scroll behavior

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -20,7 +20,7 @@ export default function ScrollUp() {
   return (
     <button
       id="ButtonUp"
-      className="opacity-0 whitespace-nowrap text-sm font-semibold h-11 w-11 fixed bottom-[84px] right-4 z-50 flex items-center justify-center rounded-full border border-[var(--bg-700)] bg-[var(--backdrop)] p-3 shadow backdrop-blur-md sm:bottom-4"
+      className="cursor-pointer opacity-0 whitespace-nowrap text-sm font-semibold h-11 w-11 fixed bottom-[84px] right-4 z-50 flex items-center justify-center rounded-full border border-[var(--bg-700)] bg-[var(--backdrop)] p-3 shadow backdrop-blur-md sm:bottom-4"
       onClick={handleScrollUp}
     >
       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
